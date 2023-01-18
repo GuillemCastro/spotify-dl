93973902bc98e4ed725a001ed803a1a385818ae7
Daniel Balagué Guardia <25331181+dbalague@users.noreply.github.com>
2023-01-18 09:42:24 +0100

    added conditional to install on macos

--- build.rs
+++ build.rs
@@ -5,8 +5,25 @@ use std::path::Path;
 fn main() {
     let library = pkg_config::Config::new().probe("flac").unwrap();
     let profile = env::var_os("PROFILE").unwrap();
-    let from = library.link_paths.get(0).unwrap().join("libFLAC.so");
-    let to = Path::new("target").join(profile).join("deps").join("libflac.so");
+
+    let lib_flac_from = String::from(
+        if env::consts::OS == "macos"{
+            "libFLAC.dylib"
+        }
+        else{
+            "libFLAC.so"
+        }
+    );
+    let lib_flac_to = String::from(
+        if env::consts::OS == "macos"{
+            "libFLAC.dylib"
+        }
+        else{
+            "libflac.so"
+        }
+    );
+    let from = library.link_paths.get(0).unwrap().join(lib_flac_from);
+    let to = Path::new("target").join(profile).join("deps").join(lib_flac_to);
     fs::copy(from, to).unwrap();
     println!("cargo:rerun-if-changed=build.rs");
 }
