29472147655f7f6780949711aaaef055481ec2e8
Guillem Castro <guillemcastro4@gmail.com>
2025-07-11 19:33:54 +0200

    chore(deps): Optimize dependencies & upgrade to 2024 edition (#33)

--- src/encoder/flac.rs
+++ src/encoder/flac.rs
@@ -2,7 +2,6 @@ use flacenc::bitsink::ByteSink;
 use flacenc::component::BitRepr;
 use flacenc::error::Verify;
 
-use super::execute_with_result;
 use super::EncodedStream;
 use super::Encoder;
 use super::Samples;
@@ -24,16 +23,11 @@ impl Encoder for FlacEncoder {
             .into_verified()
             .map_err(|e| anyhow::anyhow!("Failed to verify encoder config: {:?}", e))?;
 
-        let (tx, rx) = tokio::sync::oneshot::channel();
-
-        rayon::spawn(execute_with_result(
-            move || {
-                let flac_stream = flacenc::encode_with_fixed_block_size(
-                    &config,
-                    source,
-                    config.block_size,
-                )
-                .map_err(|e| anyhow::anyhow!("Failed to encode flac: {:?}", e))?;
+        let byte_sink: Vec<u8> =
+            tokio::task::spawn_blocking(move || -> Result<Vec<u8>, anyhow::Error> {
+                let flac_stream =
+                    flacenc::encode_with_fixed_block_size(&config, source, config.block_size)
+                        .map_err(|e| anyhow::anyhow!("Failed to encode flac: {:?}", e))?;
 
                 let mut byte_sink = ByteSink::new();
                 flac_stream
@@ -41,11 +35,8 @@ impl Encoder for FlacEncoder {
                     .map_err(|e| anyhow::anyhow!("Failed to write flac stream: {:?}", e))?;
 
                 Ok(byte_sink.into_inner())
-            },
-            tx,
-        ));
-
-        let byte_sink: Vec<u8> = rx.await??;
+            })
+            .await??;
 
         Ok(EncodedStream::new(byte_sink))
     }
--- src/encoder/mp3.rs
+++ src/encoder/mp3.rs
@@ -1,10 +1,9 @@
-use anyhow::anyhow;
 use anyhow::Ok;
+use anyhow::anyhow;
 use mp3lame_encoder::Builder;
 use mp3lame_encoder::FlushNoGap;
 use mp3lame_encoder::InterleavedPcm;
 
-use super::execute_with_result;
 use super::EncodedStream;
 use super::Encoder;
 use super::Samples;
@@ -12,11 +11,7 @@ use super::Samples;
 pub struct Mp3Encoder;
 
 impl Mp3Encoder {
-    fn build_encoder(
-        &self,
-        sample_rate: u32,
-        channels: u32,
-    ) -> anyhow::Result<mp3lame_encoder::Encoder> {
+    fn build_encoder(sample_rate: u32, channels: u32) -> anyhow::Result<mp3lame_encoder::Encoder> {
         let mut builder = Builder::new().ok_or(anyhow::anyhow!("Failed to create mp3 encoder"))?;
 
         builder
@@ -26,7 +21,7 @@ impl Mp3Encoder {
             anyhow::anyhow!("Failed to set number of channels for mp3 encoder: {}", e)
         })?;
         builder
-            .set_brate(mp3lame_encoder::Birtate::Kbps320)
+            .set_brate(mp3lame_encoder::Bitrate::Kbps320)
             .map_err(|e| anyhow::anyhow!("Failed to set bitrate for mp3 encoder: {}", e))?;
 
         builder
@@ -38,34 +33,29 @@ impl Mp3Encoder {
 #[async_trait::async_trait]
 impl Encoder for Mp3Encoder {
     async fn encode(&self, samples: Samples) -> anyhow::Result<EncodedStream> {
-        let mut mp3_encoder = self.build_encoder(samples.sample_rate, samples.channels)?;
+        let mut mp3_encoder = Self::build_encoder(samples.sample_rate, samples.channels)?;
 
-        let (tx, rx) = tokio::sync::oneshot::channel();
+        let mp3_out_buffer = tokio::task::spawn_blocking(move || {
+            let input = InterleavedPcm(samples.samples.as_slice());
+            let mut mp3_out_buffer = Vec::with_capacity(mp3lame_encoder::max_required_buffer_size(
+                samples.samples.len(),
+            ));
+            let encoded_size = mp3_encoder
+                .encode(input, mp3_out_buffer.spare_capacity_mut())
+                .map_err(|e| anyhow!("Failed to encode mp3: {}", e))?;
+            unsafe {
+                mp3_out_buffer.set_len(mp3_out_buffer.len().wrapping_add(encoded_size));
+            }
 
-        rayon::spawn(execute_with_result(
-            move || {
-                let samples: Vec<i16> = samples.samples.iter().map(|&x| x as i16).collect();
-                let input = InterleavedPcm(samples.as_slice());
-                let mut mp3_out_buffer = Vec::with_capacity(mp3lame_encoder::max_required_buffer_size(samples.len()));
-                let encoded_size = mp3_encoder
-                    .encode(input, mp3_out_buffer.spare_capacity_mut())
-                    .map_err(|e| anyhow!("Failed to encode mp3: {}", e))?;
-                unsafe {
-                    mp3_out_buffer.set_len(mp3_out_buffer.len().wrapping_add(encoded_size));
-                }
-
-                let encoded_size = mp3_encoder
-                    .flush::<FlushNoGap>(mp3_out_buffer.spare_capacity_mut())
-                    .map_err(|e| anyhow!("Failed to flush mp3 encoder: {}", e))?;
-                unsafe {
-                    mp3_out_buffer.set_len(mp3_out_buffer.len().wrapping_add(encoded_size));
-                }
-                Ok(mp3_out_buffer)
-            },
-            tx,
-        ));
-
-        let mp3_out_buffer = rx.await??;
+            let encoded_size = mp3_encoder
+                .flush::<FlushNoGap>(mp3_out_buffer.spare_capacity_mut())
+                .map_err(|e| anyhow!("Failed to flush mp3 encoder: {}", e))?;
+            unsafe {
+                mp3_out_buffer.set_len(mp3_out_buffer.len().wrapping_add(encoded_size));
+            }
+            Ok(mp3_out_buffer)
+        })
+        .await??;
 
         Ok(EncodedStream::new(mp3_out_buffer))
     }
